019c2140f97424ed3b4420001d0fe9cd14eb8025
Xia0_0 <loserxiaoke@gmail.com>
2025-01-24 23:51:17 +0800

    弹幕爬取结束

---
+++ Bilibili/Bullet/Analyze.md
@@ -0,0 +1,23 @@
+# 技术实现：
+
+## 查找Api接口：
+
+aid (AV号): 视频的AV号码。  
+bvid (BV号): BV号是B站后来引入的一种视频标识符，用来替代原有的AV号。暂时两个都有。  
+cid (Comment ID): 视频弹幕ID。  
+
+v1版本的弹幕列表接口：  
+https://api.bilibili.com/x/v1/dm/list.so?oid=CID
+用途：获取视频的所有弹幕数据，一次性返回。  
+一般就一个
+
+v2版本的分段弹幕接口：
+https://api.bilibili.com/x/v2/dm/wbi/web/seg.so?.....  
+用途：按视频的分段获取弹幕数据，适合长视频或需要按时间段管理弹幕的场景。  
+有时候接口有好几个，下图就是两个。
+
+
+检查，availableVideoList,分集视频的相关信息
+![alt text](image-1.png)
+![alt text](image-2.png)
+
--- Bilibili/Bullet/Collect.py
+++ Bilibili/Bullet/Collect.py
@@ -1,15 +1,15 @@
 '''
-用简单化的方法爬取B站视频数据,视频前加个i
+用简单化的方法爬取B站视频数据,视频前加个i,直接用别人的接口
 '''
 #导入模块
 import requests
 import json
-import re
+import re  #正则表达式模块
 
 #发送请求
-url="https://api.bilibili.com/x/v1/dm/list.so?oid=1512400877"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+url="https://api.bilibili.com/x/v1/dm/list.so?oid=1512400473"
+headers ={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0',
     'Referer': 'https://www.bilibili.com/'
 }#设置请求头
 
@@ -28,13 +28,13 @@ content_list=re.findall(pattern,html_data)
 comments='\n'.join(content_list) #列表合并字符串
 
 #输出结果
-with open('弹幕.txt','w',encoding='utf-8') as f:
+with open('弹幕.txt',mode='a',encoding='utf-8') as f:
+    f.write('\n\n\n弹幕数据：\n\n')
     f.write(comments)
 
 #输出状态码和响应内容
 print(response.status_code)  # 输出状态码，200表示成功
-print(response.text[:500])   # 输出前500个字符的响应内容，检查是否返回了数据
-
+print(html_data)  
 
 
 
